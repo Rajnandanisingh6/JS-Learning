edf647b312081637ea09e68498dab3de517e9efa
Rajnandanisingh6 <rajnandanisingh07610761@gmail.com>
2025-08-20 21:46:52 +0530

    learn loop

--- lecture4/index.js
+++ lecture4/index.js
@@ -1,1 +1,40 @@
 // loops and String
+// For Loop
+//Synatax:- for(initialisation,condotion,updation){
+ //logic
+//}
+// for(let i=1;i<=10;i++){
+//     console.log("Rajnandani");
+// }
+
+// for counting number 1 to 5
+// for(let i=1;i<=5;i++){
+//     console.log(i);
+// }
+//Reverse counting
+// for(let i=5;i>=1;i--){
+//     console.log(i);
+//}
+
+// Break --> iteration jo last ho wahi par ruk jana
+// for(let i=1;i<=6;i++){
+//     if(i ==4){
+//         break;
+//     }
+//     else{
+//         console.log(i);
+//     }
+// }
+
+//continue --> current iteration ko skip karna hai aur next iteration par jana hai
+for(let i=1;i<=6;i++){
+    if(i ==3){
+        continue;
+    }
+    else{
+        console.log(i);
+    }
+}
+
+//while loop
+
