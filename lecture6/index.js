eaca6c7d0d641f6d206bebe6cbf8a1521818ee48
Rajnandanisingh6 <rajnandanisingh07610761@gmail.com>
2025-08-24 21:20:07 +0530

    add files

---
+++ lecture6/index.js
@@ -0,0 +1,52 @@
+//Arrays
+
+
+//object --> collection of key value pair
+
+// let obj={
+//     name:"Rajnandani",
+//     age :20,
+//     weight:40,
+//     height:"5ft 4in",
+//     greet:function(){
+//         console.log("hello jee kaise ho saare");
+//     }
+// };
+// console.log(obj)
+// obj.greet();
+// console.log(typeof(obj));
+
+//homework
+//swallow copy
+//deep copy
+
+
+
+//-------------------------------------
+//Arrays --> collection of items/elements
+//syntax-->[] -- array literal
+// array constructor
+
+
+//creation of arrays
+// let arr=[1,3,4,5,6];
+// console.log(arr);
+
+//array constructor
+let brr=new Array('Raj',1,true);
+// console.log(brr);
+// console.log(brr[0]);
+
+//Build-in methods
+//push methods -->insert at end
+brr.push("singh");
+//pop --> remove last item
+brr.pop();
+
+//shift --> remove first item
+brr.shift();
+
+//unshift--> add --> first item
+brr.unshift("Rajnandani Singh");
+console.log(brr);
+
