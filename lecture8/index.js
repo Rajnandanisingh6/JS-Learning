48e9426c9ed03ce08b012a4087bc3da45d5a3d5e
Rajnandanisingh6 <rajnandanisingh07610761@gmail.com>
2025-08-31 11:06:02 +0530

    Learn Function and Block Scope

--- lecture8/index.js
+++ lecture8/index.js
@@ -6,19 +6,38 @@
 
 
 //global scope
-var age=15;
-console.log(age);
+// var age=15;
+// console.log(age);
 
-{
-    console.log(age)
-}
-if(true){
-    console.log(age)
-}
-for(let i=0;i<2;i++){
-    console.log(age);
-}
-function sayHello(){
-    console.log("hi",age);
-}
-sayHello();
+// {
+//     console.log(age)
+// }
+// if(true){
+//     console.log(age)
+// }
+// for(let i=0;i<2;i++){
+//     console.log(age);
+// }
+// function sayHello(){
+//     console.log("hi",age);
+// }
+// sayHello();
+
+
+//Function scope
+// function sayHello(){
+//     var fullname="sun";
+//     console.log("Hello dunia",fullname);
+// }
+// sayHello();
+
+//Block Scope
+// {
+//     var height=180;
+// }
+// console.log(height);
+
+//Temporal dead zone
+console.log(marks); //temporal dead zone
+let marks=100;
+console.log(marks);
