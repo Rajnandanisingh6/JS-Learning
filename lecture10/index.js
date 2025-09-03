a5e4debf2ea1e4d8e8f64f861be6dd99a8c8f3af
Rajnandanisingh6 <rajnandanisingh07610761@gmail.com>
2025-09-03 11:23:52 +0530

    Learn In-built Objects

---
+++ lecture10/index.js
@@ -0,0 +1,20 @@
+//In-built objects
+//Math object
+// console.log(Math.PI);
+// console.log(Math.max(50,555,999,44,32,43));
+// console.log(Math.min(33,44,55,66,97));
+// console.log(Math.round(1.7));
+// console.log(Math.floor(1.9));
+// console.log(Math.ceil(1.1));
+// console.log(Math.abs(-9));
+// console.log(Math.sqrt(5));
+// console.log(Math.pow(2,10));
+
+
+//Date Object
+let curr =new Date();
+console.log(curr);
+let newDate =new Date(2005, 5 ,27,4);
+console.log(newDate);
+console.log(newDate.setFullYear(2006));
+console.log(newDate);
