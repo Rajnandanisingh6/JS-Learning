84617a59d6585d9420a4a686c2dab61fe8df47b9
Rajnandanisingh6 <rajnandanisingh07610761@gmail.com>
2025-09-14 15:28:05 +0530

    learn numbers and math

--- Day2/index.js
+++ Day2/index.js
@@ -1,1 +1,21 @@
-//Strings
+//Synatax Rules for Numbers and Math
+//order of operations
+//* / are done first
+// + - are done after
+// * / have the same priority
+//2*3/5
+//calculate from left to right
+//we can use () to control which part gets done first
+//order of operations
+//()
+//* /
+// + -
+
+//1% = 1/100
+//10%= 10/100 =0.1
+
+//weired behaviour of math in javaScript
+//Math.round(2.2)
+
+
+
