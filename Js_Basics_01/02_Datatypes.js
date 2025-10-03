613d826feafbb44292eb1c4e62a3188293ef0d4b
Rajnandanisingh6 <rajnandanisingh07610761@gmail.com>
2025-10-03 21:03:22 +0530

    datatypes

---
+++ Js_Basics_01/02_Datatypes.js
@@ -0,0 +1,31 @@
+"Use strict"; // treat all js code as newer version
+
+//alert(3 +3) // we can use alert, prompt, confirm in browser only
+//we are using nodejs,not broweser
+
+//code readability should be high
+
+let name="Raj"
+let age=18
+let isLoggedIn=false
+let state;
+
+
+//primitive datatypes
+// number => 2 to power 53
+//bigint
+//string =>""
+//boolean => true/false
+//null => standalone value
+//undefined => value not assigned
+//symbol => unique value
+
+//reference datatypes => objects
+//array => []
+//object literal => {}
+//function => function(){}
+//date => new Date()
+
+//object
+
+console.log(typeof "Raj")
