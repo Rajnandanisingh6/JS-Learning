d593a032317816020a841f797c9b9a6179a6ccf1
Rajnandanisingh6 <rajnandanisingh07610761@gmail.com>
2025-10-04 19:01:53 +0530

    conversion of datatype

---
+++ Js_Basics_01/Datatype_conversion.js
@@ -0,0 +1,32 @@
+//let score=33
+//let score="33" //valueInNumber me 33 aa raha hai score ki value
+//let score="33abc" //valueInNumber me NaN aa raha hai score ki value
+//let score=null; //valueInNumber me 0 aa raha hai score ki value
+//let score=undefined; //valueInNumber me NaN aa raha hai score ki value
+//let score=true;//valueInNumber me 1 aa raha hai score ki value
+let score=false;//valueInNuumber me 0 aa raha hai score ki value
+//console.log(typeof score);
+//console.log(typeof(score));
+
+let valueInNumber=Number(score);
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
+
+//"33"=>33
+//"33abc"=>NaN
+//true=>1; false=>0
+
+let isLoggedIn=1;
+
+let booleanIsLoggedIn=Boolean(isLoggedIn);
+//console.log(booleanIsLoggedIn);
+
+//1==>true; 0=>false
+//""=>false;
+// "Raj"=>true
+
+let someNumber=44;
+
+let stringNumber=String(someNumber);
+console.log(stringNumber);
+console.log(typeof stringNumber);
