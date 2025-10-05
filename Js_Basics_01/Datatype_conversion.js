9a8d59ad582cb1b71ddbb8c28c57ff0befc402b8
Rajnandanisingh6 <rajnandanisingh07610761@gmail.com>
2025-10-05 07:24:08 +0530

    operations

--- Js_Basics_01/Datatype_conversion.js
+++ Js_Basics_01/Datatype_conversion.js
@@ -28,5 +28,37 @@ let booleanIsLoggedIn=Boolean(isLoggedIn);
 let someNumber=44;
 
 let stringNumber=String(someNumber);
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+//************************* operations ******************* 
+
+let value=4
+let negValue=-value;
+//console.log(negValue);
+
+let str1 ="hello";
+let str2 =" Raj";
+let str3 = str1 + str2;
+//console.log(str3);
+
+// console.log("1" +2);
+// console.log(1 +"2");
+// console.log("1" + 2 +2);
+// console.log(1 + 2 + "2");
+
+// console.log(+true); //1
+// console.log(+""); //0
+
+let num1, num2, num3 
+num1 =num2 = num3 =2+2
+
+let gameCounter=100
+gameCounter++;
+console.log(gameCounter);
+//postfix increment 
+x++
+//prefix increment
+++x
+
+
