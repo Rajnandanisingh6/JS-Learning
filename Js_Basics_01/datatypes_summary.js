904014b61e059bb6601ef8ecd785804d2fa50a14
Rajnandanisingh6 <rajnandanisingh07610761@gmail.com>
2025-10-05 21:13:12 +0530

    datatype summary

---
+++ Js_Basics_01/datatypes_summary.js
@@ -0,0 +1,37 @@
+//primitive 
+
+//7 types : String, Number, BigInt, Boolean, Undefined, Null, Symbol
+
+const score=100
+const scoreValue=200.3
+const isLoggedIn=false
+const Temp=null
+let userEmail;
+const id=Symbol('1234')
+const anotherId=Symbol('1234')
+console.log(id===anotherId);//false because symbol is unique
+
+const bigNumber=333334444444444488888n
+
+//Reference type (Non-primitive)
+
+//Array,Objects,Functions
+
+const heros=["shaktiman","naagraj","doga"]; //array
+//object
+let myObj ={
+    name:"Rama",
+    age:23,
+}
+//function
+const myFunction = function(){
+    console.log("Hello World");
+}
+
+console.log(typeof bigNumber);
+console.log(typeof score);
+console.log(typeof isLoggedIn);
+console.log(typeof myFunction);
+
+
+
