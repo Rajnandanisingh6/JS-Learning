2c69ca95a0a56009d67f6fa31c7f5a70ef48af7d
Rajnandanisingh6 <rajnandanisingh07610761@gmail.com>
2025-10-24 07:15:23 +0530

    add more code lines

--- Js_Basics_03/Function.js
+++ Js_Basics_03/Function.js
@@ -32,4 +32,32 @@ function loginUserMessage(username = "sam"){
 }
 
 //console.log(loginUserMessage("Rajnandani"))
-console.log(loginUserMessage("Riya"))
+//console.log(loginUserMessage("Riya"))
+
+function calculateCarPrice(val1, val2,...num1){   // ... isi ko rest operator bhi bola jata hai spread operator bhi its depands on use case
+    return num1
+}
+console.log(calculateCarPrice(200,400,600,2000))
+
+const user ={
+    username:"Rajnandani",
+    price:199
+}
+
+function handleobject(anyobject){
+    console.log(`Username is ${anyobject.username} and price is ${anyobject.price}`);
+
+}
+//handleobject(user)
+// handleobject({
+//     username:"sam",
+//     price:299
+// })
+
+const myNewArray = [200,400,100,600]
+
+function returnSecondValue(getArray){
+    return getArray[1]
+}
+//console.log(returnSecondValue(myNewArray));
+console.log(returnSecondValue([200,400,600,1000]));
