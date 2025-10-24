335354ba29e7ae23a388cb74f15ad13327e00aca
Rajnandanisingh6 <rajnandanisingh07610761@gmail.com>
2025-10-24 06:10:28 +0530

    Object part 3

---
+++ Js_Basics_02/Object2.js
@@ -0,0 +1,17 @@
+const course={
+    coursename:"Js in hindi",
+    price:"799",
+    courseInstructor:"Rajnandani"
+}
+
+const {courseInstructor:instructor} = course  //destructure
+
+//console.log(courseInstructor);
+console.log(instructor);
+
+
+// const navbar = ({company}) => {    //ye react me 
+
+// }
+// navbar(company ="Techmin")
+
