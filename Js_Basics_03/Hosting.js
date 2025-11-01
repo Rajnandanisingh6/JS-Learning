b052114f1528453f2741b31c2793c43e50622991
Rajnandanisingh6 <rajnandanisingh07610761@gmail.com>
2025-11-01 11:15:09 +0530

    hosting

---
+++ Js_Basics_03/Hosting.js
@@ -0,0 +1,14 @@
+function one(){
+    const username = "Rajnandani"
+
+    function two(){
+        const website ="youtube"
+        console.log(username);
+    }
+    console.log(website);
+
+    two()
+}
+
+one()
+
