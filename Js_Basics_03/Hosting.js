505083eb3187b872822db1cb5e8d19a28ce0ace8
Rajnandanisingh6 <rajnandanisingh07610761@gmail.com>
2025-11-03 15:04:57 +0530

    async

--- Js_Basics_03/Hosting.js
+++ Js_Basics_03/Hosting.js
@@ -10,5 +10,23 @@ function one(){
     two()
 }
 
-one()
+//one()
 
+if(true){
+    const username ="Rajnandani"
+    if(username ==="Rajnandai"){
+        const website ="youtube"
+        console.log(username  + website);
+    }
+    //console.log(website);//error because website is not defined in this scope , it is defined in inner scope 
+}
+
+//console.log(username); //error because username is not defined in this scope , it is defined in inner scope
+
+
+//+++++++++++++++++++++++
+
+
+function addone(num){
+    return num + 1
+}
