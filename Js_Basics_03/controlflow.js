3a8daedf59e5cbdb2db6ddee0bfee6f1b6ea23a8
Rajnandanisingh6 <rajnandanisingh07610761@gmail.com>
2025-11-10 05:49:33 +0530

    control flow part 1

---
+++ Js_Basics_03/controlflow.js
@@ -0,0 +1,22 @@
+//if
+// const isuserloggedIn= true
+// const temperature = 41
+// if (temperature === 40){
+//     console.log("less than 50");
+//     // code to be executed if condition is true
+// }else {
+//     console.log("temperature is greater than 50");
+// };
+// console.log("executed")
+
+
+
+// const score = 200
+
+// if(score > 100){
+//     const power = "fly"
+//     console.log(`User power: ${power}`);
+// }
+
+//console.log(`uuser power: ${power}`); // not executed
+
