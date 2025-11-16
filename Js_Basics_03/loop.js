42e4a9f23be6153ecba6f721a8987cf0ae4cf0e4
Rajnandanisingh6 <rajnandanisingh07610761@gmail.com>
2025-11-17 02:00:59 +0530

    loop

--- Js_Basics_03/loop.js
+++ Js_Basics_03/loop.js
@@ -10,12 +10,38 @@ for (let i = 0; i <= 10; i++) {
 }
 
 for (let i = 0; i <= 10; i++) {
-    console.log(`Outer loop value:${i}`);
+    ///console.log(`Outer loop value:${i}`);
     for(let j = 0; j<=10;j++){
-        console.log(`Inner loop value ${j} and inner loop ${i}`);
-    }
-    
+      //  console.log(`Inner loop value ${j} and inner loop ${i}`);
+     // console.log(i + '*' + j + '=' + i*j);
+    }  
 }
+
+let myArray =["flash","batman","superman"];
+//console.log(myArray.length);
+for (let index = 0; index < myArray.length; index++) {
+    const element = myArray[index];
+    //console.log(element); 
+}
+
+//break and continue
+
+// for (let index = 1; index <= 20; index++) {
+//     if(index == 9){
+//         console.log(`Detected 9`);
+//         break;
+//     }
+//     console.log(`value of i is ${index}`);  
+// }
+
+for (let index = 1; index <= 20; index++) {
+    if(index == 9){
+        console.log(`Detected 9`);
+        continue
+    }
+    console.log(`value of i is ${index}`);  
+}
+
 
 
 
