9f5e543aa816049917605e26b145a7b161897f3f
Rajnandanisingh6 <rajnandanisingh07610761@gmail.com>
2025-11-19 05:28:16 +0530

    loop in

---
+++ Js_Basics_03/Four.js
@@ -0,0 +1,17 @@
+const myObject={
+    js:'JasvaScript',
+    cpp:'c++',
+    rb:'Ruby',
+    py:'Python'
+}
+
+for (const key in myObject) {
+//    console.log(myObject[key]);  
+     // console.log(`${key} shortcut is for ${myObject[key]}`); 
+}
+
+const programming =["js","cpp","rb","py","java"]
+
+for (const key in programming) {
+    console.log(programming[key]);  
+}
