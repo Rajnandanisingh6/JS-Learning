211d81194251254b6e6237ea99570e9537500959
Rajnandanisingh6 <rajnandanisingh07610761@gmail.com>
2026-02-08 08:36:38 +0530

    opps

--- 10_OOPs/oop.js
+++ 10_OOPs/oop.js
@@ -4,10 +4,42 @@ const user ={
     signedIn : true,
 
     getUserDetails: function(){
-        console.log("Got user details from database");
+        //console.log("Got user details from database");
+        //console.log(`Username: ${this.username}`);
+        console.log(this);
+
     }
 }
 
 
-console.log(user.username);
-console.log(user.getUserDetails());
+//console.log(user.username);
+//console.log(user.getUserDetails());
+//console.log(this);
+
+
+//-------------------------------------------
+
+//constructor function
+
+// const promiseOne = new Promise()
+// const date = new Date()
+
+function User(username,loginCount,isLoggedIn){
+    this.username = username;
+    this.loginCount = loginCount;
+    this.isLoggedIn = isLoggedIn
+
+    return this
+}
+
+const userOne = new User("Nandani",11,true)
+const userTwo = new  User("Nandu",8,false)
+console.log(userOne);
+console.log(userTwo);
+
+//new -- empty object create hota hai / new object create ho raha hai
+// constructor function call hota hai new keyword ke karn
+// this keyword ke andar argument inject ho ja ta hai
+// function ke andar mil jate hai
+
+//instance Of
